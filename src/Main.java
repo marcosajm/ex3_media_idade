2af28583b7568a9067225d4c51bc6afbc610ce09
marcos <ilejbr@gmail.com>
2022-11-16 14:14:08 +0000

    version 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //int max = 0;
+        int sum = 0;
+        int count = 0;
+        int age, maximum=0;
+        double average= 0;
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Idade:");
+            age = sc.nextInt();
+
+            if (age <0||age > 120)
+            {
+                System.out.println("Número entre 1 to 120:");
+                //age = sc.nextInt();
+            }
+
+            else
+            {
+                count ++;
+                sum = sum + age;
+                maximum = getMax(maximum, age);
+            }
+        }while(age != 0);
+        average = getAve(sum, count);
+        System.out.println("Max é:" + maximum + "\nMédia é:" + average);
+
     }
+
+    public static int getMax (int max, int age)
+    {
+        if (max < age)
+        {
+            max = age;
+        }
+        return max;
+    }
+
+    public static double getAve (int sum, int count)
+    {
+        double average;
+        average = (double)sum / (double)count;
+        return average;
+    }
+
 }
